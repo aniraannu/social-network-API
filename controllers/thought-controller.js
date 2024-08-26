17bb955cd2270b843c23513f64e1ab22ae9afb85
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-26 14:28:17 -0700

    Added a method to create a new reaction and delete a reaction using id

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -91,4 +91,34 @@ const thoughtController = {
         })
           .catch((err) => res.status(400).json(err));
     },
+    //POST to create a reaction
+    addReaction({ params, body }, res) {
+      Thought.findOneAndUpdate(
+        { _id: params.thoughtId },
+        { $push: { reactions: body } },
+        { new: true, runValidators: true }
+      )
+        .then((dbUserData) => {
+          if (!dbUserData) {
+            res.status(404).json({ message: "No user found with this id!" });
+            return;
+          }
+          res.json(dbUserData);
+        })
+        .catch((err) => res.status(400).json(err));
+    },
+    //DELETE a reaction
+    removeReaction({ params }, res) {
+      Thought.findOneAndUpdate(
+        //when reaction id matches with the params.reactionId passed
+        { _id: params.thoughtId },
+        //remove a specific reply from reactions
+        { $pull: { reactions: { reactionId: params.reactionId } } },
+        { new: true }
+      )
+        .then((dbThoughtData) => res.json(dbThoughtData))
+        .catch((err) => res.json(err));
+    },
 };
+//Export thoughts controller
+module.exports = thoughtController;
