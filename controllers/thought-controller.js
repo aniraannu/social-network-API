b1c4762c34ef0917b7fdd79da635cd20520156a3
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-26 14:06:18 -0700

    Added  a POST method to create a new thought in the thoughts controller

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -1,22 +1,54 @@
 //Import thoughts and user from models
-const { Thought, User } = require('../models');
+const { Thought, User } = require("../models");
 //Create a thought controller
 const thoughtController = {
-    //Get all thoughts
-    getAllThoughts(req, res) {
-        Thought.find({})
-            .then(dbThoughtData => res.json(dbThoughtData))
-            .catch(err => {
-                console.log(err);
-                res.status(400).json(err);
-            });
-    },
-    //Get a single thought by id
-    getThoughtById({ params }, res) {
-        Thought.findOne({ _id: params.id })
-            .then(dbThoughtData => {
-                if (!dbThoughtData) {
-                    res.status(404).json({ message: 'No thought found with this id!' });
-                    return;
-                }
-                res.json(dbThoughtData);
+  //Get all thoughts
+  getAllThoughts(req, res) {
+    Thought.find({})
+      .populate({
+        path: "thoughts",
+      })
+      .sort({ _id: -1 })
+      .then((dbThoughtData) => res.json(dbThoughtData))
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+  //Get a single thought by id
+  getThoughtById({ params }, res) {
+    Thought.findOne({ _id: params.thoughtId })
+      .populate({
+        path: "thoughts",
+      })
+      .then((dbThoughtData) => {
+        if (!dbThoughtData) {
+          res.status(404).json({ message: "No thought found with this id!" });
+          return;
+        }
+        res.json(dbThoughtData);
+      })
+      .catch((err) => {
+        res.status(400).json(err);
+      });
+  },
+  //POST to create a new thought
+  createThought({ params, body }, res) {
+    Thought.create(body)
+      .then(({ _id }) => {
+        console.log(_id);
+        return User.findOneAndUpdate(
+          { _id: body.userId },
+          { $push: { thoughts: _id } },
+          { new: true }
+        );
+      })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: "No user found with this id!" });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.json(err));
+  },
+};
