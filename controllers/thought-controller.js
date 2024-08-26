15c25148503198455263117fa42a46fdac671b6e
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-26 13:57:55 -0700

    Created thoughts controller and added methods to get all thoughts and get asingle thought by id

---
+++ controllers/thought-controller.js
@@ -0,0 +1,22 @@
+//Import thoughts and user from models
+const { Thought, User } = require('../models');
+//Create a thought controller
+const thoughtController = {
+    //Get all thoughts
+    getAllThoughts(req, res) {
+        Thought.find({})
+            .then(dbThoughtData => res.json(dbThoughtData))
+            .catch(err => {
+                console.log(err);
+                res.status(400).json(err);
+            });
+    },
+    //Get a single thought by id
+    getThoughtById({ params }, res) {
+        Thought.findOne({ _id: params.id })
+            .then(dbThoughtData => {
+                if (!dbThoughtData) {
+                    res.status(404).json({ message: 'No thought found with this id!' });
+                    return;
+                }
+                res.json(dbThoughtData);
