7e9a20a0340d7e6810e5c2b6ebddb7110951df44
aniraannu <51684910+aniraannu@users.noreply.github.com>
2024-08-26 14:40:17 -0700

    Added a date formatting tool in the utils folder

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 const router = require('express').Router();
 // import all API routes from /api/index.js
 const apiRoutes = require("./api");
-
+//Use the /api prefix for all API routes
 router.use("/api", apiRoutes);
 
 router.use((req, res) => {
     res.status(404).send('<h1>404 Error!</h1>');
   });
-  
+//Export routes
   module.exports = router;
